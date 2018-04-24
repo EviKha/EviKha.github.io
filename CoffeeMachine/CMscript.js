c22ad75c499d8a6e4ce1423506a5f9f8289a6972
abk182 <abk1612@gmail.com>
2018-04-24 17:08:44 +0300

    relize version v1.0.0.1-beta

--- CoffeeMachine/CMscript.js
+++ CoffeeMachine/CMscript.js
@@ -1,43 +1,53 @@
 class CoffeeMachine {
 	constructor(coffeeArray) {
 		console.log(coffeeArray)
-	this.coffeeArray = coffeeArray
-
+		this.coffeeArray = coffeeArray
+		this.render();
 	}
-
 	render() {
 		var countSum = 0
-
-		var conteinerIdRoot = document.getElementById('root');
-		console.log(coffeeArray)
+		var conteinerIdRoot = document.getElementById('coffeeList');
 		this.coffeeArray.forEach(function(element){
 			conteinerIdRoot.innerHTML 
 			+= "<p>"
-			+ element.name + " : "	+ element.price + " "
-			+ "<button id ="+ element.id +">" + "Please" + "</button>"
+			+ "<button id ="+ element.id +">"
+			+  element.name + " : "	+ element.price + " " + "</button>"
 			+ "</p>"
 		})
 		var conteinerIdSelect = document.getElementById('selectedcoffee');
 		this.coffeeArray.forEach(function(element){
 			document.getElementById(element.id).addEventListener("click", function(){
-				conteinerIdSelect.innerHTML += "<p>" + element.name + " : "+ element.price  + "</p>"
-			countSum += element.price
-				document.getElementById("end").innerHTML = countSum
+				conteinerIdSelect.innerHTML = "<p>" + element.name + " : "+ element.price  + "</p>"
+				countSum += element.price
+				document.getElementById("end").innerHTML ="total: " + countSum
 			});
-
 		})
+		var buttonBut = document.getElementById('but');
+		var input = document.getElementById("cashInput");
+		buttonBut.addEventListener("click", function(){
+			console.log(countSum);
+			console.log(input.value);
+			var delivery = input.value - countSum;
+			input.value = delivery;
+			countSum = 0;
+			document.getElementById("end").innerHTML ="total:0 "
 
-
+		});
 	}
 }
 
 var coffeeArray = [
 	{name: 'capuchino', price : 10, id : "cup"},
 	{name: "americano", price : 5,  id : "ame"},
-	{name: "chai", price : 20,  id : "chai"}
+	{name: "chai", price : 20,  id : "chai"},
+	{name: "water", price: 0, id: "water"}
 	]
 var CoffeeMachineObj = new CoffeeMachine(coffeeArray)
-CoffeeMachineObj.render()
+// CoffeeMachineObj.render()
 console.log(CoffeeMachineObj)
 
-//косруктор то что строит обьект. в фигурных скобках это входные параметры который принимает в себя обьект
+
+
+
+
+//косруктор то что строит обьект. в круглых скобках это входные параметры которые принимает в себя обьект, в фигурныхэто тело функции(класс)
